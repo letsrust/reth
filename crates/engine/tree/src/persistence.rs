4426588b5767cd4a1a3afade8b6cb281266de12b
Dan Cline <6798349+Rjected@users.noreply.github.com>
2024-11-11 18:20:04 -0500

    wip: disable errors for consistent view provider

--- crates/engine/tree/src/persistence.rs
+++ crates/engine/tree/src/persistence.rs
@@ -85,6 +85,9 @@ impl<N: PersistenceNodeTypes> PersistenceService<N> {
                     let result = self.on_save_blocks(blocks)?;
                     let result_number = result.map(|r| r.number);
 
+                    // we ignore the error because the caller may or may not care about the result
+                    let _ = sender.send(result);
+
                     if let Some(block_number) = result_number {
                         // send new sync metrics based on saved blocks
                         let _ = self
@@ -96,9 +99,6 @@ impl<N: PersistenceNodeTypes> PersistenceService<N> {
                             let _ = self.prune_before(block_number)?;
                         }
                     }
-
-                    // we ignore the error because the caller may or may not care about the result
-                    let _ = sender.send(result);
                 }
                 PersistenceAction::SaveFinalizedBlock(finalized_block) => {
                     let provider = self.provider.database_provider_rw()?;
